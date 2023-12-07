980ec37f4881c7a51ab0242fdf06b043392341d6
LEGION <besherjumaa@gmail.com>
2023-12-07 15:30:23 +0300

    2nd change

--- lib/test.dart
+++ lib/test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Testing extends StatelessWidget {
-  Testing({Key key}) : super(key: key);
+  const Testing({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
