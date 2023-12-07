96ad4a77a08cc470ababd43e426e3ac3d2fc9ea8
LEGION <besherjumaa@gmail.com>
2023-12-07 15:03:16 +0300

    test

---
+++ lib/test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Testing extends StatelessWidget {
+  Testing({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const Scaffold(
+      body: Text("Whatever"),
+    );
+  }
+}
